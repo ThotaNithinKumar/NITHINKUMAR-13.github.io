68f6aa1cd7d06b51cd27fe9f3139edf5fbef53f7
NITHINKUMAR-13 <88588888+NITHINKUMAR-13@users.noreply.github.com>
2021-08-18 21:39:04 +0530

    Add files via upload

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,7 @@
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,7 @@
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+	window.helloSpeaker = helloSpeaker;
+})(window);
